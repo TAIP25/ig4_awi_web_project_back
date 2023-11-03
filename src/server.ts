0f8c51b0f718d2a3149e0f42dbbf23b6e2d07386
Robin <robin.ave28@gmail.com>
2023-11-03 15:54:49 +0100

    Update server console log to include URL

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ app.get('/test', async (_req: any, res: any) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
 main()
