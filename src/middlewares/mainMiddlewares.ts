4a00d2bd5d1a5c5a451c800cc419df7eba280d46
Robinkss <robin.ave28@gmail.com>
2024-02-05 00:14:32 +0100

    Update prod

--- src/middlewares/mainMiddlewares.ts
+++ src/middlewares/mainMiddlewares.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 export const configCors = cors(
     {
-        origin: "https://festival-du-jeu-1u44.onrender.com", // allow all origins
+        origin: "*", // allow all origins
         
     }
 );
