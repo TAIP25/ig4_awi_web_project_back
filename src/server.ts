f3fb6102d1c3db84cde3fccfc3d7b3f18d986196
Robinkss <robin.ave28@gmail.com>
2024-02-04 18:39:49 +0100

    Update prod

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import { NODE_ENV, PORT } from "./config.js";
+import { PrismaClient } from "@prisma/client";
+
+
+import app from "./app.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.$connect();
+    console.log("Database connected");
+}
+
+app.get('/', (_req: any, res: any) => {
+    res.send(`Hello, we are in the ${NODE_ENV} environment`);
+});
+
+app.get('/test', async (_req: any, res: any) => {
+    const benevoles = await prisma.benevole.findMany();
+    res.json(benevoles);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
