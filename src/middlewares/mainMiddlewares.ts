bb72402c9a6b98a962b750d443eb57bd078b1979
Robinkss <robin.ave28@gmail.com>
2024-02-04 20:42:28 +0100

    Update prod

--- src/middlewares/mainMiddlewares.ts
+++ src/middlewares/mainMiddlewares.ts
@@ -1,3 +1,9 @@
 import cors from 'cors';
 
-export const configCors = cors()
+export const configCors = cors(
+    {
+        origin: true,
+        credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    }
+);
